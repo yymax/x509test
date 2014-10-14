50dcd116c711f335e7c11c5216f36e91b3eb14e3
Calvin Liang <onlinesite@hotmail.com>
2014-10-13 17:51:52 -0700

    Update x509test.py comment

--- x509test.py
+++ x509test.py
@@ -1,6 +1,6 @@
 """
 This file contains the entry point for the software. Users should initiate
-the program in a terminal by typing "python3 Run.py"
+the program in a terminal by typing "python3 x509test.py"
 
 @author: Calvin Jia Liang
 Created on Sep 3, 2014
